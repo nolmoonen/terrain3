8bac44029a9d31b06cf863205037d4c7670a1792
nolmoonen <nolmoonen@hotmail.nl>
2024-07-07 19:54:11 +0200

    download imgui and stb on configure

--- src/stb_wrapper.cpp
+++ src/stb_wrapper.cpp
@@ -1,10 +1,10 @@
 #define STB_IMAGE_IMPLEMENTATION
 
-#include "../external/stb/stb_image.h"
+#include <stb_image.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
-#include "../external/stb/stb_image_write.h"
+#include <stb_image_write.h>
 #include "nmutil/log.h"
 
 int write_png(char const *filename, int x, int y, int comp, const void *data)
